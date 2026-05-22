099c46e81d5b6395f5cf9c15d53cf01819679a20
Julian_abi <juliansepulveda0206@gmail.com>
2026-05-22 17:35:07 -0500

    feat: enhance user dashboard and sidebar with profile information and improved layout

--- src/app/(profesor)/home/page.tsx
+++ src/app/(profesor)/home/page.tsx
@@ -1,3 +1,97 @@
-export default function Page() {
-  return <h1>teacher Dashboard</h1>
+import { User, GraduationCap, Shield, Phone, Mail, Divide, IdCard, UserRoundKey } from "lucide-react"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import { getSesion } from "@/features/auth/actions/get-sesion"
+
+export default async function Page() {
+
+    const perfil = await getSesion();
+    if (!perfil.sesion) {
+        return(<h1 className="text-4xl font-bold tracking-tight">
+            No se pudo cargar la información del perfil
+        </h1>)
+    }
+
+    return (
+        <div className="space-y-8 p-8">
+            <div className="space-y-2">
+                <h1 className="text-4xl font-bold tracking-tight">
+                    Bienvenido al Panel de Profesor
+                </h1>
+                <p className="text-muted-foreground text-lg">
+                    Aquí puede administrar cursos, estudiantes y más. 
+                    Use el menú de navegación para acceder a las diferentes secciones del panel.
+                </p>
+            </div>
+            <div className="space-y-2">
+                <Separator />
+                <div className="pt-4">
+                    <h2 className="text-2xl font-semibold">
+                        Información del Perfil
+                    </h2>
+                    <p className="text-muted-foreground">
+                        Información general del profesor
+                    </p>
+                </div>
+            </div>
+            <Card className="shadow-xl bg-[#3d1f8c]">
+                <CardContent className="pt-6">
+                    <div className="grid md:grid-cols-2 gap-6">
+                        <div className="flex items-center gap-4 p-4 rounded-xl border bg-white">
+                            <div className="bg-violet-100 p-3 rounded-full">
+                                <User className="text-[#3d1f8c]" />
+                            </div>
+                            <div>
+                                <p className="text-sm text-[#3d1f8c] font-bold ">
+                                    Nombre Completo
+                                </p>
+                                <h3 className="text-lg">
+                                    {perfil.sesion.name}
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 rounded-xl border bg-white">
+                            <div className="bg-violet-100 p-3 rounded-full">
+                                <UserRoundKey className="text-[#3d1f8c]" />
+                            </div>
+                            <div>
+                                <p className="text-sm text-[#3d1f8c] font-bold">
+                                    Rol
+                                </p>
+                                <h3 className="text-lg">
+                                    {perfil.sesion.role}
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 rounded-xl border bg-white">
+                            <div className="bg-violet-100 p-3 rounded-full">
+                                <IdCard className="text-[#3d1f8c]" /> </div>
+                            <div>
+                                <p className="text-sm text-[#3d1f8c] font-bold">
+                                    Cedula
+                                </p>
+                                <h3 className="text-lg">
+                                    {perfil.sesion.cedula}
+                                </h3>
+                            </div>
+                        </div>
+                        <div className="flex items-center gap-4 p-4 rounded-xl border bg-white">
+                            <div className="bg-violet-100 p-3 rounded-full ">
+                                <Mail className="text-[#3d1f8c]" />
+                            </div>
+                            <div>
+                                <p className="text-sm text-[#3d1f8c] font-bold">
+                                    Email
+                                </p>
+
+                                <h3 className="text-lg">
+                                    {perfil.sesion.email}
+                                </h3>
+                            </div>
+                        </div>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -17,9 +17,10 @@ import { useRouter } from 'next/navigation'
 import { Separator } from '../ui/separator'
 import Link from 'next/link'
 import { rutesAdmin, rutesStudent, rutesTeacher } from './routes'
+import Image from 'next/image'
 
 type AppSidebarProps = {
-  role: 'student' | 'teacher' | 'admin'  
+  role: 'student' | 'teacher' | 'admin'
 }
 
 type content = {
@@ -43,45 +44,65 @@ export function AppSidebar({ role }: AppSidebarProps) {
     }
   }
   return (
-    <Sidebar collapsible="icon">
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            {/* <SidebarMenuItem className="flex items-center ">
-              <div className="size-10 flex items-center justify-center bg-purple-600 rounded-full mr-3">
-                <span className="text-2xl">🎓</span>
+    <Sidebar
+      collapsible="icon"
+      className="border-r-0"
+    >
+      <div className="h-full flex flex-col justify-between bg-[#3d1f8c]">
+        <SidebarContent className="flex-1">
+          <SidebarGroup>
+            <SidebarGroupContent>
+              <div className="bg-white rounded-xl p-3 flex items-center gap-3 shadow-md transition-all">
+                <Image
+                  src="/logo.svg"
+                  alt="Logo"
+                  width={70}
+                  height={70}
+                  className="shrink-0"
+                />
+                <div className="group-data-[collapsible=icon]:hidden">
+                  <h1 className="font-bold text-[#3d1f8c] text-lg">
+                    Virtual Box
+                  </h1>
+                  <p className="text-black text-sm">
+                    Panel 
+                    {role === 'admin' ? ' Administrativo' : role === 'teacher' ? ' Profesor' : ' Estudiante'}
+                  </p>
+                </div>
               </div>
-              <div>
-                <h1 className="text-xl font-bold">MobiLab UA</h1>
-                <span>{name}</span>
-              </div>
-            </SidebarMenuItem> */}
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup>
-          <Separator></Separator>
-          <SidebarGroupLabel>Contenido</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {rutes.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <item.icon />
-                      <span className="text-md">{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenuButton onClick={handleLogout}>
-          <LogOut /> Cerrar Sesión
-        </SidebarMenuButton>
-      </SidebarFooter>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup>
+            <Separator></Separator>
+            <SidebarGroupLabel className="text-white text-lg font-bold mt-4 mb-2">
+              Contenido
+            </SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {rutes.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild className="text-white hover:bg-white rounded-lg">
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span className="text-md">{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenuButton
+            onClick={handleLogout}
+            className="text-white hover:bg-red-600 hover:text-white transition rounded-lg"
+          >
+            <LogOut />
+            Cerrar Sesión
+          </SidebarMenuButton>
+        </SidebarFooter>
+      </div>
     </Sidebar>
   )
 }
